e7132cf176b22bdffc7bd97929d68dff19949733
chonai-flora <kaito.goforward0912@gmail.com>
2023-03-05 17:50:35 +0900

    Remove deploy directory

--- src/firebase/firebase-modules.ts
+++ src/firebase/firebase-modules.ts
@@ -11,7 +11,7 @@ const db = getFirestore(firebaseApp);
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
 const appCheck = initializeAppCheck(firebaseApp, {
-  provider: new ReCaptchaV3Provider('6LcyXlEkAAAAAF4AG1iMPN61XCPERsAFYOBzPSnv'),
+  provider: new ReCaptchaV3Provider(''),
 
   // Optional argument. If true, the SDK automatically refreshes App Check
   // tokens as needed.
